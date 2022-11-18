475758faef2aadbfa8da8febb2e435d37f746353
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-11-18 11:41:27 +0000

    kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,35 @@
 class BooksController < ApplicationController
 
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] ="You have created book successfully."
+    redirect_to book_path(@book)
+    else
+      
+     render :index
+    end
+  end
+
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
